e7cd2aced4842e56699af42b50711362c0179caf
german salas <german.salas.mt@correo.usa.edu.co>
2021-10-13 19:35:36 -0500

    inicio

---
+++ src/main/java/germansalas/test1/proyectociclo3/Proyectociclo3Application.java
@@ -0,0 +1,13 @@
+package germansalas.test1.proyectociclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectociclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectociclo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/germansalas/test1/proyectociclo3/Proyectociclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package germansalas.test1.proyectociclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectociclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
