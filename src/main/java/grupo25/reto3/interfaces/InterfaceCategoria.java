f0bf9e52ea4a736c0f1119318c831c6c36ab1287
germansalas1994 <germansalas1103@hotmail.com>
2021-11-02 14:40:31 -0500

    pomxml editado

--- src/main/java/grupo25/reto3/interfaces/InterfaceCategoria.java
+++ src/main/java/grupo25/reto3/interfaces/InterfaceCategoria.java
@@ -1,20 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- package grupo25.reto3.interfaces;
+package grupo25.reto3.interfaces;
 
 import org.springframework.data.repository.CrudRepository;
 import grupo25.reto3.modelo.Categoria;
